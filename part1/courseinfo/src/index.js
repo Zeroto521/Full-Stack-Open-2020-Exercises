c59b03d156cbdc68d0f1b3a6346b6e8651e15fb5
Zero <zeroto521@gmail.com>
2020-12-02 23:07:42 +0800

    :tada: finish 1.5 exercise

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,27 +7,29 @@ import { Total } from './components/Total'
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total exercises={parts.map(x => x['exercises'])} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts.map(x => x['exercises'])} />
     </div>
   )
 }
