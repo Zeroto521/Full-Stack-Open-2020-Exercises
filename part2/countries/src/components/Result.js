88d879f710dc0317e41d4b2bd1dca8ec37d6e47b
Zero <Zeroto521@gmail.com>
2020-12-09 09:10:44 +0800

    Update Result.js

--- part2/countries/src/components/Result.js
+++ part2/countries/src/components/Result.js
@@ -9,26 +9,34 @@ const Result = (props) => {
   const url = `https://restcountries.eu/rest/v2/name/${searchName}`
 
   useEffect(() => {
-    axios.get(url).then(response => {
-      setCountries(response.data)
-    })
+    if (searchName) {
+      axios.get(url).then(response => {
+        setCountries(response.data)
+      })
+    }
   })
 
+  if (searchName) {
+    return (
+      <div>
+        {
+          (countries.length === 1) &&
+          <Country country={countries[0]} />
+        }
+        {
+          countries.length >= 2 && countries.length <= 10 &&
+          <div>{countries.map(e => <div key={e.name}>{e.name}</div>)}</div>
+        }
+        {
+          countries.length > 10 &&
+          <div>Too many matches, specify another filter</div>
+        }
+      </div>
+    )
+  }
+
   return (
-    <div>
-      {
-        countries.length === 1 &&
-        <Country country={countries[0]} />
-      }
-      {
-        countries.length >= 2 && countries.length <= 10 &&
-        <div>{countries.map(e => <div>{e.name}</div>)}</div>
-      }
-      {
-        countries.length > 10 &&
-        <div>Too many matches, specify another filter</div>
-      }
-    </div>
+    <div></div>
   )
 }
 
