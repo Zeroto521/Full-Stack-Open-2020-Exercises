3e6f1d14713aafe3e5dc6ca49b6f53093382a009
Zero <Zeroto521@gmail.com>
2020-12-09 13:18:02 +0800

    :tada: finish 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import { Button } from './Button'
 
 const Country = (props) => {
   const { country, countryShow, setShow } = props
+  const [weatherIcon, setWeatherIcon] = useState('')
+  const [temperature, setTemperature] = useState('')
+  const [windSpeed, setWindSpeed] = useState('')
+  const [windDirection, setWindDirection] = useState('')
+
+
+  const api_key = process.env.REACT_APP_API_KEY
+  const weather_api = `http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`
+
+  useEffect(() => {
+    axios.get(weather_api).then(response => {
+      setWeatherIcon(response.data['current']['weather_icons'][0])
+      setTemperature(response.data['current']['temperature'])
+      setWindSpeed(response.data['current']['wind_speed'])
+      setWindDirection(response.data['current']['wind_dir'])
+    })
+  }, [weather_api])
 
   if (countryShow[country.name]) {
     return (
@@ -20,6 +40,23 @@ const Country = (props) => {
         <br />
         <div>
           <img src={country.flag} alt="Country Flag" width="8%" />
+        </div>
+        <div>
+          <h3>Weather in {country.capital}</h3>
+          <div>
+            <div>
+              <h4>temperature</h4>
+              <div>
+                {temperature} Celcius
+              </div>
+              <img src={weatherIcon} alt="Weather Icon" width="5%" />
+            </div>
+            <div>
+              <h4>wind</h4>
+              <div>Speed: {windSpeed} mph</div>
+              <div>Direction: {windDirection}</div>
+            </div>
+          </div>
         </div>
       </div>
     )
