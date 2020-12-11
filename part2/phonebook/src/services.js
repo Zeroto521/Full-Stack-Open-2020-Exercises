819aef5d179c988e1bdaeaf004891c58a93a725a
Zero <Zeroto521@gmail.com>
2020-12-11 11:35:38 +0800

    :tada: finish 3.11

--- part2/phonebook/src/services.js
+++ part2/phonebook/src/services.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://agile-refuge-83919.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
